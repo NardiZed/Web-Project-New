6ddab7ea17a492ad7d92bdfc4f8f2d0a9d1305c0
aklesya23 <tewobstya233@gmail.com>
2025-09-25 16:23:46 +0300

    added a gitignore file to ignore committing packages installed using npm, created a new index file with a simple starter code to check if it's working and also installed all basic backend packages

---
+++ BACK_END/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
